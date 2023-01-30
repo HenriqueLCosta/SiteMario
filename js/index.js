94888b74b26510597a2f719275e6c1498d4365f0
Henrique Luis <henriqueluiss84@gmail.com>
2023-01-30 15:05:00 -0300

    finalizacão

--- js/index.js
+++ js/index.js
@@ -1,10 +1,14 @@
 
-alert("OLa mundo");
-
-
-
 const botaoTrailer = document.querySelector(".botao-trailer");
-
+const fecharmodal = document.querySelector(".fechar-modal");
 botaoTrailer.addEventListener("click", () => {
-    alert("clicou no botao")
+    let abrir = document.querySelector(".modal");
+    abrir.classList.add("modal-aberto");
+
+})
+
+fecharmodal.addEventListener("click", () => {
+    let fechar = document.querySelector(".modal");
+    fechar.classList.remove("modal-aberto");
+    
 })
